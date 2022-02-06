cc5017f4d16fb09b07e9fccbd99589fcc408d418
Lubov <2l-u-b-o-v@ukr.net>
2022-02-06 11:21:28 +0200

    promises - возвращает промис и результат выполнения промиса

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  if (delay.value < 0 || step.value < 0 || amount.value <= 0) {
+    Notify.failure(`❌ Incorrect values`);
+    return;
   }
+
+  let resultDelay = parseInt(delay.value);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    const promise = createPromise(i, resultDelay);
+    resultPromise(promise);
+    resultDelay += parseInt(step.value);
+  }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function resultPromise(promise) {
+  promise
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
